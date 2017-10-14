0fac23c22307ca598e0cc6712280903c2a7d559d
N.C Wu <mythnc@gmail.com>
2017-10-14 23:51:10 +0800

    Improve auto battle module

--- gbf_bot/auto_battle.py
+++ gbf_bot/auto_battle.py
@@ -2,10 +2,13 @@ import logging
 import random
 import time
 import pyautogui
+from . import top_left, window_size
 from . import auto_battle_config as config
+from . import utility
 from .components import Button
 
 logger = logging.getLogger(__name__)
+
 
 attack = Button('attack.png', config['attack'])
 auto = Button('auto.png', config['auto'])
@@ -13,7 +16,17 @@ auto = Button('auto.png', config['auto'])
 
 def activate(battle_time):
     pyautogui.PAUSE = 1.3
-    time.sleep(5 + random.random() * 0.25)
+
+    # wait before battle start
+    w, h = window_size
+    start_pt = (top_left[0] + w//2, top_left[1] + h*1//3)
+    region = start_pt + (w//2, h*2//3)
+    while True:
+        time.sleep(0.5)
+        found = utility.locate(attack.path, region)
+        if found is not None:
+            break
+
     logger.info('click attack')
     attack.double_click()
     time.sleep(random.random() * 0.35)
@@ -21,4 +34,4 @@ def activate(battle_time):
     auto.click()
 
     # battle result
-    time.sleep(battle_time + random.random() * 3)
+    time.sleep(battle_time)
