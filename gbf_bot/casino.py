550ca1b2149dbc0c7fb8c80553287318f171bcc2
N.C Wu <mythnc@gmail.com>
2017-10-21 18:31:05 +0800

    Add poker play()
    * Add a new rule: 4 kind (suit version)

--- gbf_bot/casino.py
+++ gbf_bot/casino.py
@@ -1,13 +1,106 @@
+import random
+
 class Poker:
     card_match = "XX23456789TJQKA"
 
     def __init__(self, cards):
         self.cards = cards
-        self.numbers = [Poker.card_match.index(c[1:]) 
+        self.numbers = [Poker.card_match.index(c[1:])
                         for c in cards if c != "JOKER"]
         self.numbers.sort()
         self.suits = [c[:1] for c in cards if c != "JOKER"]
         self.has_joker = "JOKER" in cards
+        self.logger = logging.getLogger(__name__ + '.' + Poker.__name__)
+
+    def play(self):
+        '''return index of keep cards
+        '''
+        if self.has_joker:
+            return self.play_with_joker()
+
+        if self.is_straight_flush():
+            self.logger.info('straight flush')
+            result = list(range(5))
+        elif self.kind(4):
+            self.logger.info('4 kind')
+            result = self.index_all(self.kind(4))
+        elif self.is_full_house():
+            self.logger.info('full house')
+            result = list(range(5))
+        elif self.is_flush():
+            self.logger.info('flush')
+            result = list(range(5))
+        elif self.is_straight():
+            self.logger.info('straight')
+            result = list(range(5))
+        elif self.kind(3):
+            self.logger.info('3 kind')
+            result = self.index_all(self.kind(3))
+        elif self.two_pair():
+            self.logger.info('2 pair')
+            pairs = self.two_pair()
+            indexes_2d = [self.index_all(pair) for pair in pairs]
+            result = [n for index in indexes_2d for n in index]
+        elif self.kind(2):
+            self.logger.info('one pair')
+            result = self.index_all(self.kind(2))
+        elif self.kind(4, 'suits'):
+            self.logger.info('4 kind of suits')
+            result = self.index_all(self.kind(4, 'suits'))
+        else:
+            self.logger.info('no pair: random chose one or drop all')
+            i = random.randint(0, 1)
+            # drop all or random chose 1
+            select = [[], [random.randint(0, 4)]]
+            result = select[i]
+
+        self.logger.info(result)
+        return result
+
+    def play_with_joker(self):
+        joker_index = [self.cards.index('JOKER')]
+
+        interval = self.numbers[-1] - self.numbers[0]
+        # 5 kind
+        if self.kind(4) and self.has_joker:
+            self.logger.info('5 kind')
+            result = list(range(5))
+        elif self.is_straight_flush():
+            self.logger.info('straight flush')
+            result = list(range(5))
+        # 4 kind
+        elif self.kind(3) and self.has_joker:
+            self.logger.info('4 kind')
+            result = self.index_all(self.kind(3)) + joker_index
+        # full house
+        elif self.two_pair() or self.kind(3):
+            self.logger.info('full house')
+            result = list(range(5))
+        elif self.is_flush():
+            self.logger.info('flush')
+            result = list(range(5))
+        # straight
+        elif (self.is_straight() or (len(set(self.numbers)) == 4
+                                     and (interval == 3 or interval == 4))):
+            self.logger.info('straight')
+            result = list(range(5))
+        # 3 kind
+        elif self.kind(2):
+            self.logger.info('3 kind')
+            result = self.index_all(self.kind(2)) + joker_index
+        else:
+            self.logger.info('no pair: chose joker')
+            result = joker_index
+        return result
+
+    def index_all(self, x):
+        # numbers
+        if isinstance(x, int):
+            string = Poker.card_match[x]
+        # suits
+        elif isinstance(x, str):
+            string = x
+        return [i for i, card in enumerate(self.cards) if string in card]
 
     def two_pair(self):
         pair1 = self.kind(2)
@@ -16,13 +109,14 @@ class Poker:
             return (pair1, pair2)
         return None
 
-    def kind(self, size, is_reversed=False):
-        numbers = self.numbers
+    def kind(self, size, cmp='numbers', is_reversed=False):
+        d = {'numbers': self.numbers, 'suits': self.suits}
+        compare = d[cmp]
         if is_reversed:
-            numbers = reversed(self.numbers)
-        for number in numbers:
-            if self.numbers.count(number) == size:
-                return number
+            compare = reversed(compare)
+        for c in compare:
+            if d[cmp].count(c) == size:
+                return c
         return None
 
     def is_straight(self):
--- tests/test_casino.py
+++ tests/test_casino.py
@@ -2,6 +2,71 @@ from gbf_bot.casino import Poker
 
 
 class TestPoker:
+    def test_play(self):
+        # two pair
+        poker = Poker(('S2', 'D2', 'H3', 'C3', 'C5'))
+        assert poker.play() == [0, 1, 2, 3]
+        # 3 kind
+        poker = Poker(('S2', 'D2', 'H2', 'D4', 'C5'))
+        assert poker.play() == [0, 1, 2]
+        poker = Poker(('S2', 'D2', 'JOKER', 'D4', 'C5'))
+        assert poker.play() == [0, 1, 2]
+        # straight
+        poker = Poker(('S2', 'D3', 'H6', 'D4', 'C5'))
+        assert poker.play() == list(range(5))
+        poker = Poker(('S2', 'D3', 'HA', 'D4', 'C5'))
+        assert poker.play() == list(range(5))
+        poker = Poker(('ST', 'DQ', 'HA', 'DK', 'CJ'))
+        assert poker.play() == list(range(5))
+        poker = Poker(['S6', 'D2', 'JOKER', 'C4', 'C5'])
+        assert poker.play() == list(range(5))
+        poker = Poker(['SA', 'H3', 'JOKER', 'C4', 'C5'])
+        assert poker.play() == list(range(5))
+        poker = Poker(['SA', 'DJ', 'JOKER', 'CK', 'CT'])
+        assert poker.play() == list(range(5))
+        # flush
+        poker = Poker(['S2', 'H2', 'S3', 'H3', 'C3'])
+        assert poker.play() == list(range(5))
+        poker = Poker(['S6', 'S2', 'JOKER', 'S4', 'S5'])
+        assert poker.play() == list(range(5))
+        # full house
+        poker = Poker(['S2', 'H2', 'JOKER', 'S3', 'H3'])
+        assert poker.play() == list(range(5))
+        poker = Poker(['D3', 'H2', 'JOKER', 'S3', 'D2'])
+        assert poker.play() == list(range(5))
+        # 4 kind
+        poker = Poker(['S5', 'D5', 'H5', 'JOKER', 'C3'])
+        assert poker.play() == [0, 1, 2, 3]
+        poker = Poker(['S2', 'H2', 'JOKER', 'S3', 'D2'])
+        assert poker.play() == [0, 1, 4, 2]
+        # straight flush
+        poker = Poker(['SA', 'S2', 'JOKER', 'S3', 'S4'])
+        assert poker.play() == list(range(5))
+        # 5 kind
+        poker = Poker(['S5', 'D5', 'H5', 'C5', 'JOKER'])
+        assert poker.play() == list(range(5))
+        # joker
+        poker = Poker(['S5', 'D6', 'HA', 'C8', 'JOKER'])
+        assert poker.play() == [4]
+        # one pair
+        poker = Poker(['S5', 'D6', 'HA', 'C8', 'D5'])
+        assert poker.play() == [0, 4]
+        poker = Poker(('H5', 'D6', 'C7', 'C5', 'S3'))
+        assert poker.play() == [0, 3]
+        # 4 kind suits
+        poker = Poker(['S5', 'S6', 'SA', 'S8', 'D9'])
+        assert poker.play() == [0, 1, 2, 3]
+        # no pair
+        poker = Poker(['S5', 'D6', 'HA', 'C8', 'CK'])
+        n = len(poker.play())
+        assert n == 1 or n == 0
+
+    def test_index_all(self):
+        poker = Poker(('S2', 'D2', 'H3', 'C3', 'C5'))
+        assert poker.index_all(2) == [0, 1]
+        poker = Poker(('S3', 'D3', 'H3', 'C3', 'C5'))
+        assert poker.index_all(3) == [0, 1, 2, 3]
+
     def test_two_pair(self):
         poker = Poker(('S2', 'D2', 'H3', 'C3', 'C5'))
         assert poker.two_pair() == (2, 3)
@@ -19,6 +84,10 @@ class TestPoker:
         assert poker.kind(4) == 5
         poker = Poker(['S5', 'D5', 'CK', 'C5', 'C3'])
         assert poker.kind(4) is None
+
+    def test_4_kind_suits(self):
+        poker = Poker(['S5', 'S9', 'SA', 'S2', 'C3'])
+        assert poker.kind(4, 'suits') == 'S'
         
     def test_is_straight(self):
         poker = Poker(['S6', 'D2', 'H3', 'C4', 'C5'])
